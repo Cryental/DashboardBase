4e650b8a270de908ebd57e0aec7b082467b3872e
Cryental <contact@cryental.dev>
2023-05-02 10:10:04 +0600

    import optimizations

--- app/Http/Controllers/Auth/CaptchaController.php
+++ app/Http/Controllers/Auth/CaptchaController.php
@@ -3,12 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
-use Laravel\Socialite\Facades\Socialite;
 
 class CaptchaController extends Controller
 {
