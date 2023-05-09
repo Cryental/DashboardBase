6b9f4e9b1eccc08a948deb4cd34737ee1206556c
Cryental <contact@cryental.dev>
2023-05-09 19:27:48 +0600

    only 7 days

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class DashboardController extends Controller
 {
@@ -20,7 +21,10 @@ class DashboardController extends Controller
 
     public function getUsersChartData()
     {
+        $sevenDaysAgo = Carbon::now()->subDays(7)->toDateString();
+
         $users = User::selectRaw('DATE(created_at) as date, COUNT(*) as count')
+            ->whereDate('created_at', '>=', $sevenDaysAgo)
             ->groupBy('date')
             ->orderBy('date')
             ->get();
