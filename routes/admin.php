f44335ce4b0bda3653f58be3e6934580532dde46
Cryental <contact@cryental.dev>
2023-05-09 19:11:55 +0600

    route prefix added

--- routes/admin.php
+++ routes/admin.php
@@ -13,35 +13,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth', 'verified', 'level:2'])->group(function () {
-    Route::get('/admin/users-chart-data', [\App\Http\Controllers\Admin\DashboardController::class, 'getUsersChartData']);
-    Route::get('/admin', [\App\Http\Controllers\Admin\DashboardController::class, 'show'])
-        ->name('admin.dashboard');
-    Route::get('/admin/users', [\App\Http\Controllers\Admin\UserController::class, 'show'])
-        ->name('admin.users');
-    Route::post('/admin/users/search', [\App\Http\Controllers\Admin\UserController::class, 'search'])
-        ->name('admin.users.search');
-    Route::post('/admin/users', [\App\Http\Controllers\Admin\UserController::class, 'store']);
-    Route::post('/admin/users/{id}/remove', [\App\Http\Controllers\Admin\UserController::class, 'delete']);
-    Route::get('/admin/users/{id}', [\App\Http\Controllers\Admin\UserController::class, 'edit']);
-    Route::post('/admin/users/{id}', [\App\Http\Controllers\Admin\UserController::class, 'editSave']);
-    Route::get('/admin/users/{id}/remove_device/{device_id}', [\App\Http\Controllers\Admin\UserController::class, 'logoutDevice']);
+Route::prefix('admin')->group(function () {
+    Route::middleware(['auth', 'verified', 'level:2'])->group(function () {
+        Route::get('/users-chart-data', [\App\Http\Controllers\Admin\DashboardController::class, 'getUsersChartData']);
+        Route::get('/', [\App\Http\Controllers\Admin\DashboardController::class, 'show'])
+            ->name('admin.dashboard');
+        Route::get('/users', [\App\Http\Controllers\Admin\UserController::class, 'show'])
+            ->name('admin.users');
+        Route::post('/users/search', [\App\Http\Controllers\Admin\UserController::class, 'search'])
+            ->name('admin.users.search');
+        Route::post('/users', [\App\Http\Controllers\Admin\UserController::class, 'store']);
+        Route::post('/users/{id}/remove', [\App\Http\Controllers\Admin\UserController::class, 'delete']);
+        Route::get('/users/{id}', [\App\Http\Controllers\Admin\UserController::class, 'edit']);
+        Route::post('/users/{id}', [\App\Http\Controllers\Admin\UserController::class, 'editSave']);
+        Route::get('/users/{id}/remove_device/{device_id}', [\App\Http\Controllers\Admin\UserController::class, 'logoutDevice']);
 
-    Route::get('/admin/roles', [\App\Http\Controllers\Admin\RoleController::class, 'show'])
-        ->name('admin.roles');
-    Route::post('/admin/roles', [\App\Http\Controllers\Admin\RoleController::class, 'store']);
-    Route::post('/admin/roles/search', [\App\Http\Controllers\Admin\RoleController::class, 'search'])
-        ->name('admin.roles.search');
-    Route::get('/admin/roles/{id}', [\App\Http\Controllers\Admin\RoleController::class, 'edit']);
-    Route::post('/admin/roles/{id}/remove', [\App\Http\Controllers\Admin\RoleController::class, 'delete']);
-    Route::post('/admin/roles/{id}', [\App\Http\Controllers\Admin\RoleController::class, 'editSave']);
+        Route::get('/roles', [\App\Http\Controllers\Admin\RoleController::class, 'show'])
+            ->name('admin.roles');
+        Route::post('/roles', [\App\Http\Controllers\Admin\RoleController::class, 'store']);
+        Route::post('/roles/search', [\App\Http\Controllers\Admin\RoleController::class, 'search'])
+            ->name('admin.roles.search');
+        Route::get('/roles/{id}', [\App\Http\Controllers\Admin\RoleController::class, 'edit']);
+        Route::post('/roles/{id}/remove', [\App\Http\Controllers\Admin\RoleController::class, 'delete']);
+        Route::post('/roles/{id}', [\App\Http\Controllers\Admin\RoleController::class, 'editSave']);
 
-    Route::get('/admin/permissions', [\App\Http\Controllers\Admin\PermissionController::class, 'show'])
-        ->name('admin.permissions');
-    Route::post('/admin/permissions', [\App\Http\Controllers\Admin\PermissionController::class, 'store']);
-    Route::post('/admin/permissions/search', [\App\Http\Controllers\Admin\PermissionController::class, 'search'])
-        ->name('admin.permissions.search');
-    Route::get('/admin/permissions/{id}', [\App\Http\Controllers\Admin\PermissionController::class, 'edit']);
-    Route::post('/admin/permissions/{id}/remove', [\App\Http\Controllers\Admin\PermissionController::class, 'delete']);
-    Route::post('/admin/permissions/{id}', [\App\Http\Controllers\Admin\PermissionController::class, 'editSave']);
+        Route::get('/permissions', [\App\Http\Controllers\Admin\PermissionController::class, 'show'])
+            ->name('admin.permissions');
+        Route::post('/permissions', [\App\Http\Controllers\Admin\PermissionController::class, 'store']);
+        Route::post('/permissions/search', [\App\Http\Controllers\Admin\PermissionController::class, 'search'])
+            ->name('admin.permissions.search');
+        Route::get('/permissions/{id}', [\App\Http\Controllers\Admin\PermissionController::class, 'edit']);
+        Route::post('/permissions/{id}/remove', [\App\Http\Controllers\Admin\PermissionController::class, 'delete']);
+        Route::post('/permissions/{id}', [\App\Http\Controllers\Admin\PermissionController::class, 'editSave']);
+    });
 });
