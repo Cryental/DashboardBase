87db89b4bf3b137c7d74f8f0c83b9b8452edd411
Cryental <contact@cryental.dev>
2023-05-10 14:45:25 +0600

    adding missing libs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call(PermissionsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(ConnectRelationshipsSeeder::class);
-        //$this->call(UsersTableSeeder::class);
+//        $this->call(UsersTableSeeder::class);
 
         Model::reguard();
     }
